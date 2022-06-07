15184194fc752879a94f6da65b9f08e8a0bf1bf6
Fatima Nasser <fatimahnasser1999@gmail.com>
2022-06-07 11:51:50 +0300

    renamed index component

--- src/components/IndexList.js
+++ src/components/IndexList.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../App.css';
 
-const Index = (props) => {
+const IndexList = (props) => {
 	function displayIndex(index) {
 		let className = index === props.selectedIndex ? 'indexSelected' : 'index';
 		return <button className={className}>{index}</button>;
@@ -9,4 +9,4 @@ const Index = (props) => {
 	return <div className='indexList'>{props.indexList.map(displayIndex)}</div>;
 };
 
-export default Index;
+export default IndexList;
